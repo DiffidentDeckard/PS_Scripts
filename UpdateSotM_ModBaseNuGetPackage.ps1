9a5c46d71668580d1176bc82347e38e67ee539ec
DiffidentDeckard <DiffidentDeckard@gmail.com>
2022-10-12 15:17:10 -0400

    Reverted to previous method, changed .nupkg to .zip

--- UpdateSotM_ModBaseNuGetPackage.ps1
+++ UpdateSotM_ModBaseNuGetPackage.ps1
@@ -12,19 +12,32 @@ $SentinelsEngine = "SentinelsEngine"
 [System.Console]::WriteLine("Artifact Staging Directory: $ArtifactStagingDirectory")
 [System.Console]::WriteLine("Downloads directory: $DownloadsDirectory")
 [System.Console]::WriteLine("$ILRepack Download URL: $IlrDownloadUrl")
-	
-# Install ILRepack
-[System.Console]::WriteLine("Installing $ILRepack...")
+
+# Download ILRepack through a web client
 $ilrDirectory = "$DownloadsDirectory\$ILRepack"
-Install-Package -Name $ILRepack -Source "https://api.nuget.org/v3/index.json" -Destination $ilrDirectory
+$ilrNupkg = "$ilrDirectory.zip"
+[System.Console]::WriteLine("Downloading $SteamCmd...")
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($IlrDownloadUrl, $ilrNupkg)
 	
-# Make sure ILRepack was installed
+# Make sure ILRepack was downloaded
+if(!(Test-Path $ilrNupkg -PathType Leaf))
+{
+	throw [System.IO.FileNotFoundException] "$ilrNupkg"
+}
+[System.Console]::WriteLine("Successfully downloaded $ILRepack")
+	
+# Unzip ILRepack
+[System.Console]::WriteLine("Extracting files from $ilrNupkg...")
+Expand-Archive -Path $ilrNupkg -Destination $ilrDirectory -Force
+	
+# Make sure ILRepack was unzipped
 $ilrExe= "$ilrDirectory\tools\$ILRepack.exe"
 if(!(Test-Path $ilrExe -PathType Leaf))
 {
 	throw [System.IO.FileNotFoundException] "$ilrExe"
 }
-[System.Console]::WriteLine("Successfully installed $ILRepack")
+[System.Console]::WriteLine("Successfully extracted files from $ilrNupkg")
 
 # Run ILRepack to merge all the output .dll and .pdb files
 [System.Console]::WriteLine("Running ILRepack...")
